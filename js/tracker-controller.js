43a407de038d733e0b20ae62cacd59d3a26d4e14
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-05-19 15:35:37 +0800

    Add tracker-controller.js

---
+++ js/tracker-controller.js
@@ -0,0 +1,18 @@
+// Insert your javascript/jquery code here
+			
+// This will store all the expenses locally
+var expenses = [];
+var sum = 0;
+
+$(document).ready(function() {
+
+	// This creates a date object with an initialized value of the date today
+	var today = new Date();
+	var formattedDate = today.getFullYear().toString() + '-' + (today.getMonth() + 1).toString().padStart(2, 0) + '-' + today.getDate().toString().padStart(2, 0);
+
+	// Set the formattedDate value to the date input field's value
+
+
+	// Place all functions and validations here
+    
+});
