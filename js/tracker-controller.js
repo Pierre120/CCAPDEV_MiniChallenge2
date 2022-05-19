d4e3bd545c736e24b85de09dee74258bbdbcf25e
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-05-19 15:53:32 +0800

    Add function for setting the date input's value to the current date

--- js/tracker-controller.js
+++ js/tracker-controller.js
@@ -6,13 +6,28 @@ var sum = 0;
 
 $(document).ready(function() {
 
-	// This creates a date object with an initialized value of the date today
-	var today = new Date();
-	var formattedDate = today.getFullYear().toString() + '-' + (today.getMonth() + 1).toString().padStart(2, 0) + '-' + today.getDate().toString().padStart(2, 0);
+	// Clicking the submit button
+	$('#submit').click(function() {
+		// Form validation
 
-	// Set the formattedDate value to the date input field's value
+		// Adding of item in item list
+	});
+
+	// Set the date input field's value to current date
+	setCurrentDate();
 
 
 	// Place all functions and validations here
+
+	// For setting the date value of `#date` to current date
+	function setCurrentDate() {
+		// This creates a date object with an initialized value of the date today
+		var today = new Date();
+		var formattedDate = today.getFullYear().toString() + '-' + (today.getMonth() + 1).toString().padStart(2, 0) + '-' + today.getDate().toString().padStart(2, 0);
+		console.log(formattedDate);
+
+		// Set the formattedDate value to the date input field's value
+		$('#date').val(formattedDate);
+	}
     
 });
